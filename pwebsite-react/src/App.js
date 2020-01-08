6e5e11f6dcf2d00ca12b5f71dc4fc3aca123f1ef
Andrew Hsu <andrewhsu94@gmail.com>
2020-01-07 20:37:36 -0600

    fundamental misunderstanding of what ghpages is

--- pwebsite-react/src/App.js
+++ pwebsite-react/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 import HomepageImage from './components/HomepageImage.js'
 import pdf from './components/Andrew_Hsu_Resume.pdf'
-import {HashRouter} from "react-router-dom"
-
 
 function App() {
   return (
-    <HashRouter basename ='/'>
+    
     <div className="App">
       <header className="App-header">
         <HomepageImage/>
         <p>
           Andrew Hsu
+          <br></br>
+          Software Engineer
         </p>
+        
         <a
           className="App-link"
           href="https://www.linkedin.com/in/andrew-hsu-297965b2/"
@@ -25,7 +26,7 @@ function App() {
         <a href = {pdf}>Resume</a>
       </header>
     </div>
-    </HashRouter>
+    
   );
 }
 
